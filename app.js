be79991d56fd38b5a9624b2109742bee6bd94311
raulo2032 <raulaggio@gmail.com>
2022-05-29 20:19:50 -0300

    subo todos los archivos

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const publicFolderPath = path.resolve(__dirname, './public');
+app.use(express.static(publicFolderPath));
+
+app.listen(3000, ()=>{
+    console.log("Servidor corriendo");
+});
+app.get('/', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, './views/home.html'))
+})
