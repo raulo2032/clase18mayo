d3e434822ba22294a04a2771e9b37675c57f7892
raulo2032 <raulaggio@gmail.com>
2022-05-29 20:29:21 -0300

    codigo ninja

--- app.js
+++ app.js
@@ -6,9 +6,6 @@ const app = express();
 const publicFolderPath = path.resolve(__dirname, './public');
 app.use(express.static(publicFolderPath));
 
-app.listen(3000, ()=>{
-    console.log("Servidor corriendo");
-});
-app.get('/', (req,res)=>{
-    res.sendFile(path.resolve(__dirname, './views/home.html'))
+app.listen(3000, ()=>{    console.log("Servidor corriendo");        });
+app.get('/', (req,res)=>{    res.sendFile(path.resolve(__dirname, './views/home.html'))
 })
